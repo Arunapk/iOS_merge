17d3419838d0a6b7135886ac34efac04eaefd527
Munish <muniyaraj2315@gmail.com>
2025-05-05 12:35:12 +0530

    inhouse inital added

---
+++ iOS_inhouse_work/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  iOS_inhouse_work
+//
+//  Created by ihub on 29/04/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ iOS_inhouse_work/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iOS_inhouse_work
+//
+//  Created by ihub on 29/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .orange
+    }
+
+
+}
+
