1efe9861a368cc86cdefa8232cbe1a8150fa7489
Arunpandikumar <arunpandikumarm@appkodes.com>
2025-05-05 13:27:45 +0530

    viewChangedToRed

--- iOS_inhouse_work/ViewController.swift
+++ iOS_inhouse_work/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .orange
+        view.backgroundColor = .red
     }
 
 
