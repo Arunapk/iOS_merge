8b58ee3ce1be0fbe5c02dbed19fc9deeb2ff3d20
Arunpandikumar <arunpandikumarm@appkodes.com>
2025-05-14 12:34:42 +0530

    setupInitVC

--- iOS_inhouse_work/SceneDelegate.swift
+++ iOS_inhouse_work/SceneDelegate.swift
@@ -17,14 +17,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        let window = UIWindow(windowScene: windowScene)
-        let nav = UINavigationController(rootViewController: StateController())
-        window.rootViewController = nav
-        window.makeKeyAndVisible()
-        
-        self.window = window
-        
+        self.setupWindow(using: windowScene)
+    }
+    
+    private func setupWindow(using windowScene: UIWindowScene) {
+        window = UIWindow(windowScene: windowScene)
+        setInitialViewController(vc: StateController())
+        window?.makeKeyAndVisible()
+    }
+    
+    func setInitialViewController(vc: UIViewController){
+        let nav = UINavigationController(rootViewController: vc)
+        window?.rootViewController = nav
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
